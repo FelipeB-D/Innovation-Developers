b069cc947cd4a2a164adfcd69d3a006f47fe6ff3
AIC <104699297+AngelaIC@users.noreply.github.com>
2022-08-26 15:22:46 -0500

    Add files via upload

---
+++ proyecto/src/main/java/edu/udea/main/model/MovimientoDinero.java
@@ -0,0 +1,12 @@
+package edu.udea.main.model;
+
+public class MovimientoDinero {
+
+    private int montoMovimiento;
+    private int conceptoDelMovimiento;
+
+    public MovimientoDinero(int montoMovimiento, int conceptoDelMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+        this.conceptoDelMovimiento = conceptoDelMovimiento;
+    }
+}
