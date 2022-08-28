3f7256320673ee1773b6304d382d963f4153298b
AIC <104699297+AngelaIC@users.noreply.github.com>
2022-08-27 20:10:23 -0500

    New changes in business

--- proyecto/src/main/java/edu/udea/main/business/GestorEmpleado.java
+++ proyecto/src/main/java/edu/udea/main/business/GestorEmpleado.java
@@ -1,4 +1,33 @@
 package edu.udea.main.business;
 
+import edu.udea.main.model.Empleado;
+
+import java.util.ArrayList;
+
 public class GestorEmpleado {
+    private ArrayList<Empleado> empleados;
+
+    public GestorEmpleado(){
+        this.empleados = new ArrayList<>();
+        this.empleados.add(new Empleado("Angela", "kimypunchos@gmail.com","Innovation Developers","Srum Master"));
+        this.empleados.add(new Empleado("Luis", "luisalbertovalencia1966@gmail.com", "Innovation Developers","Administrador de BD"));
+        this.empleados.add(new Empleado("Oscar", "oscar.rvalencia@hotmail.com","Innovation Developers"," Product Owner"));
+        this.empleados.add(new Empleado("Felipe","felpebe660@hotmail.com","Innovation Developers","Analista"));
+        this.empleados.add(new Empleado("Jorge","kinnorvisual123@gmail.com","Innovation Developers","Desarrollador"));
+    }
+    public Empleado getEmpleado(String nombreEmpleado){
+        for (Empleado empleados:this.empleados){
+            if (Empleado.getNombreEmpleado().equals(nombreEmpleado)){
+                return Empleado;
+            }
+        }
+    }
+
+    public ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(ArrayList<Empleado> empleados) {
+        this.empleados = empleados;
+    }
 }
