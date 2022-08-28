11f4c2044959f2e6b328cc6811d51ebe6f342e84
POO <oscar.rvalencia@hotmail.com>
2022-08-28 11:53:43 -0500

    Desarrollo en Gestor y controlador Empleado

--- proyecto/src/main/java/edu/udea/main/business/GestorEmpleado.java
+++ proyecto/src/main/java/edu/udea/main/business/GestorEmpleado.java
@@ -1,4 +1,25 @@
 package edu.udea.main.business;
 
+import edu.udea.main.model.Empleado;
+
+import java.util.ArrayList;
+
 public class GestorEmpleado {
+
+    public ArrayList<Empleado> empleados;
+
+    public GestorEmpleado(){
+
+        this.empleados = new ArrayList<>();
+
+        this.empleados.add(new Empleado("Angela", "angela@id.com", "InnovationsDevelopers", "administradora" ));
+    }
+
+    public ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(ArrayList<Empleado> empleados) {
+        this.empleados = empleados;
+    }
 }
---
+++ proyecto/src/main/java/edu/udea/main/controlador/EmpleadoControlador.java
@@ -0,0 +1,23 @@
+package edu.udea.main.controlador;
+
+
+import edu.udea.main.business.GestorEmpleado;
+import edu.udea.main.model.Empleado;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public  class EmpleadoControlador {
+
+    private GestorEmpleado gestorEmpleado = new GestorEmpleado();
+
+    @GetMapping("/empleado")
+    public ResponseEntity<ArrayList<Empleado>> getEmpleados(){
+        return new ResponseEntity<>(gestorEmpleado.getEmpleados(), HttpStatus.ACCEPTED);
+    }
+
+}
